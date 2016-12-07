4cd6562d33789b3bb06dbae44929954f16e9d866
pfzuo <pfzuo@hust.edu.cn>
2016-12-07 12:25:37 +0800

    1.0

---
+++ BEES_server/direct_upload.h
@@ -0,0 +1,15 @@
+/*
+ * direct upload  
+ * author: Jie Wu(courageJ)
+*/
+
+#include <iostream>
+#include <cstdio>
+ #include <string>
+using namespace std;
+
+string GetFileName(); // name new files based on the current time
+ssize_t Readn(int fd, void *vptr, size_t n); // read the data stream from the client
+void WriteImagetoFile(int connfd) ; // receive th data stream of images and save
+
+#define Default_Orb_File 16000 
